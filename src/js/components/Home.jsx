1ee5cd46cf31989b5c1820fe522246f061221749
FelipeFlores <110569081+erfeli@users.noreply.github.com>
2025-05-08 20:23:45 +0000

    finalizar alarma

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [seconds, setSeconds] = useState(0);
+	const [intervalId, setIntervalId] = useState();
+	
+
+	useEffect(() => {
+		startInterval()
+	}, [])
+
+	const startInterval = () => {
+		if (intervalId) return;
+		const id = setInterval(() => {
+			setSeconds(prevSecond => prevSecond + 1)
+		}, 100);
+		setIntervalId(id)
+	}
+	 . 
+	const stopInterval = () => {
+		if (intervalId) {
+			clearInterval(intervalId)
+			setIntervalId(0)
+		}
+	}
+	const resetCounter = () => {
+		clearInterval(intervalId)
+		setSeconds(0)
+		setIntervalId(null)
+		
+	}
+
+	const alarmCounter = () => {
+		if (seconds === alarmCounter(0)){
+			return alert ("finalizado")
+		}
+	}
+
+
+
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			<h1 className="text-center mt-5">Counter: {seconds} s</h1>
+			<button onClick={startInterval}>Start</button>
+			<button onClick={stopInterval}>Stop</button>
+			<button onClick={resetCounter}>Reset</button>
+			<input type="number" value={""}  onChange={alarmCounter}/>
+
+
+
+
+
 		</div>
 	);
 };
