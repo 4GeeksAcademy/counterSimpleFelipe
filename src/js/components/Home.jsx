485b8d3e8cd385fe9fc640d6b8cfc1df9d906284
FelipeFlores <110569081+erfeli@users.noreply.github.com>
2025-05-18 03:13:01 +0000

    finalizado contador

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,20 +4,23 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
 	const [seconds, setSeconds] = useState(0);
 	const [intervalId, setIntervalId] = useState();
-	
+	const [alarmValue, setAlarmValue] = useState(null);
+
+
 
 	useEffect(() => {
 		startInterval()
 	}, [])
 
+
 	const startInterval = () => {
 		if (intervalId) return;
 		const id = setInterval(() => {
 			setSeconds(prevSecond => prevSecond + 1)
-		}, 100);
+		}, 1000);
 		setIntervalId(id)
 	}
-	 . 
+
 	const stopInterval = () => {
 		if (intervalId) {
 			clearInterval(intervalId)
@@ -28,31 +31,53 @@ const Home = () => {
 		clearInterval(intervalId)
 		setSeconds(0)
 		setIntervalId(null)
-		
+
 	}
 
+	const backCounter = () => {
+		if (intervalId) return;
+		const id = setInterval(() => {
+			setSeconds(prevSecond => {
+				if (prevSecond <= 1) {
+					clearInterval(id);
+					return 0;
+				}
+				return prevSecond - 1;
+			});
+		}, 1000);
+		setIntervalId(id);
+	};
+
 	const alarmCounter = () => {
-		if (seconds === alarmCounter(0)){
-			return alert ("finalizado")
+		if (seconds === alarmCounter(0)) {
+			alert("finalizado")
 		}
 	}
+
+	useEffect(() => {
+		if (seconds === alarmValue) {
+			alert("¡Finalizado!");
+		}
+	}, [seconds, alarmValue]);
+
+	 const formattedSeconds = String(seconds).padStart(6, "0");
+
 
 
 
 	return (
 		<div className="text-center">
-
-
-			<h1 className="text-center mt-5">Counter: {seconds} s</h1>
+			<h1 className="text-center m-5 display-1 text-bg-dark p-3">🕑{formattedSeconds}</h1>
 			<button onClick={startInterval}>Start</button>
 			<button onClick={stopInterval}>Stop</button>
 			<button onClick={resetCounter}>Reset</button>
-			<input type="number" value={""}  onChange={alarmCounter}/>
-
-
-
-
-
+			<button onClick={backCounter}>Cuenta Atrás</button>
+			<input
+				type="number"
+				value={alarmValue || ""}
+				placeholder="Introduce número"
+				onChange={(e) => setAlarmValue(Number(e.target.value))}
+			/>
 		</div>
 	);
 };
